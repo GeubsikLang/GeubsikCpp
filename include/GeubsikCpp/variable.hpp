32e1ebd5738864d29297c252861c13120a452a84
static <kmc7468@naver.com>
2019-01-15 13:37:13 +0900

    variable 클래스 추가

---
+++ include/GeubsikCpp/variable.hpp
@@ -0,0 +1,22 @@
+#ifndef GEUBSIKCPP_HEADER_VARIABLE_HPP
+#define GEUBSIKCPP_HEADER_VARIABLE_HPP
+
+#include <variant>
+
+namespace gs
+{
+	enum class variable_type
+	{
+		undefined,
+		number,
+		string,
+		function,
+	};
+
+	class variable
+	{
+		// TODO
+	};
+}
+
+#endif
